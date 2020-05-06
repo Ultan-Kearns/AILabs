7ef3194f5bdc387d14f96db4fc6229fc2a2ca523
Ultan <ultankearns@gmail.com>
2020-05-06 02:51:05 +0100

    AI Lab 4 done
    This is the fourth AI lab finished

---
+++ AILab4/src/ai/GunRunner.java
@@ -0,0 +1,36 @@
+package ai;
+
+import ie.gmit.sw.ai.nn.BackpropagationTrainer;
+import ie.gmit.sw.ai.nn.NeuralNetwork;
+import ie.gmit.sw.ai.nn.Utils;
+import ie.gmit.sw.ai.nn.activator.Activator;
+
+public class GunRunner {
+	private double[][] data = { // Health, Sword, Gun, Enemies
+			{ 2, 0, 0, 0 }, { 2, 0, 0, 1 }, { 2, 0, 1, 1 }, { 2, 0, 1, 2 }, { 2, 1, 0, 2 }, { 2, 1, 0, 1 },
+			{ 1, 0, 0, 0 }, { 1, 0, 0, 1 }, { 1, 0, 1, 1 }, { 1, 0, 1, 2 }, { 1, 1, 0, 2 }, { 1, 1, 0, 1 },
+			{ 0, 0, 0, 0 }, { 0, 0, 0, 1 }, { 0, 0, 1, 1 }, { 0, 0, 1, 2 }, { 0, 1, 0, 2 }, { 0, 1, 0, 1 } };
+
+	private double[][] expected = { // Panic, Attack, Hide, Run
+			{ 0.0, 0.0, 1.0, 0.0 }, { 0.0, 0.0, 1.0, 0.0 }, { 1.0, 0.0, 0.0, 0.0 }, { 1.0, 0.0, 0.0, 0.0 },
+			{ 0.0, 0.0, 0.0, 1.0 }, { 1.0, 0.0, 0.0, 0.0 }, { 0.0, 0.0, 1.0, 0.0 }, { 0.0, 0.0, 0.0, 1.0 },
+			{ 1.0, 0.0, 0.0, 0.0 }, { 0.0, 0.0, 0.0, 1.0 }, { 0.0, 0.0, 0.0, 1.0 }, { 0.0, 0.0, 0.0, 1.0 },
+			{ 0.0, 0.0, 1.0, 0.0 }, { 0.0, 0.0, 0.0, 1.0 }, { 0.0, 0.0, 0.0, 1.0 }, { 0.0, 1.0, 0.0, 0.0 },
+			{ 0.0, 1.0, 0.0, 0.0 }, { 0.0, 0.0, 0.0, 1.0 } };
+
+	GunRunner() throws Exception {
+		NeuralNetwork nn = new NeuralNetwork(Activator.ActivationFunction.Sigmoid, 4,3, 4);
+		BackpropagationTrainer trainer = new BackpropagationTrainer(nn);
+		trainer.train(Utils.normalize(data, 0, 100), expected, 0.01, 10000);
+		int testIndex = 11;
+		double[] result = nn.process(data[testIndex]);
+		for (int i = 0; i < expected[testIndex].length; i++) {
+			System.out.print(expected[testIndex][i] + ",");
+		}
+		System.out.println("==>" + (Utils.getMaxIndex(result) + 1));
+	}
+
+	public static void main(String[] args) throws Exception {
+		new GunRunner();
+	}
+}
